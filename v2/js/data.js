f824faa2e08c3484d9831dec18207b4777966da9
Spyros Garyfallos <paloukari@hotmail.com>
2019-07-21 11:25:52 -0700

    added filtering

--- v2/js/data.js
+++ v2/js/data.js
@@ -15,23 +15,44 @@ Array.prototype.unique = function () {
     return arr;
 }
 
+jQuery.fn.visible = function () {
+    return this.css('visibility', 'visible');
+};
+
+var allData = [];
+var filteredData = [];
+
 var countries = [];
-function loadData() {
-    d3.csv('data/database.csv', ready);
+var words = [];
+
+function refresh() {
+    if (allData === null || allData.length == 0)
+        d3.csv('data/database.csv', initializeOptions);
 }
 
-function ready(error, data) {
 
+
+function initializeOptions(error, data) {
+    allData = data;
+    filteredData = data;
     // autocomplete country
-    let countries = data.map(a => a.country).unique();
-    autocomplete(document.getElementById("country"), countries);
+    countries = data.map(a => a.country).unique();
+
+    var country = document.getElementById("country");
+    country.options = countries.map(function (c) {
+        var option = document.createElement("option");
+        option.text = c;
+        option.value = c;
+        country.appendChild(option);
+    })
+
 
     // autocomplete keywords
-    let words = data.map(
+    words = data.map(
         a => a.review.split(/(\s+)/).map(
             function (e) {
                 //remove punctuation 
-                return e.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+                return e.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
             }
         ).filter(
             // remove small words
@@ -39,7 +60,38 @@ function ready(error, data) {
         )
     )
     words = words.flat()
-
     words = words.unique();
-    autocomplete(document.getElementById("keywords"), words);
+
+    var keywords = document.getElementById("keywords");
+    keywords.options = words.map(function (c) {
+        var option = document.createElement("option");
+        option.text = c;
+        option.value = c;
+        keywords.appendChild(option);
+    })
+
+    $('.chosen-select').chosen({}).trigger("chosen:updated");
+    $('.chosen-select').visible()
+
+    $(".chosen-select").chosen({}).change(function (e, c) {
+        selectedCountries = Array.from(document.getElementById("country").selectedOptions).map(function (e) {
+            return e.value;
+        });
+        selectedKeywords = Array.from(document.getElementById("keywords").selectedOptions).map(function (e) { return e.value; });
+
+        filteredData = allData.filter(function (e) {
+            if (selectedCountries != null && selectedCountries.length > 0) {
+                if (!selectedCountries.includes(e.country))
+                    return false;
+            }
+
+            if (selectedKeywords != null && selectedKeywords.length > 0) {
+                if (!selectedKeywords.some(function (v) { return e.review.toLowerCase().indexOf(v) >= 0; })) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+    });
 }
