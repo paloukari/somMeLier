64f679099e93e8e295126751a05d847762ecd92f
Eduarda Espindola <eduarda.espindola@berkeley.edu>
2019-07-27 17:42:24 -0300

    Flask Version

---
+++ v4/static/js/choropleth.js
@@ -0,0 +1,207 @@
+var wine_viz_lib = wine_viz_lib || {};
+
+wine_viz_lib.choropleth = function () {
+  var width = 700;
+  var height = 350;
+  var data = d3.map();
+  var graph_data = [];
+
+  var svg = d3
+    .select("div#choropleth")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  var svg2 = d3
+    .select("div#choropleth")
+    .append("svg")
+    .attr("width", 280)
+    .attr("height", 250)
+    .style("display", "inline-block")
+    .style("padding-left", "5px");
+
+  var add_highlight = function (d) {
+    svg2
+      .append("text")
+      .attr("id", "curr_country_name")
+      .attr("x", 0)
+      .attr("y", 150)
+      .style("font-size", 18)
+      .style("font-weight", "bold")
+      .text(function () {
+        return d.properties.name;
+      });
+    svg2
+      .append("text")
+      .attr("id", "curr_country_grape")
+      .attr("x", 0)
+      .attr("y", 180)
+      .style("font-size", 14)
+      .text(function () {
+        return "Type: " + d.obj.grape;
+      });
+    svg2
+      .append("text")
+      .attr("id", "curr_country_rating")
+      .attr("x", 0)
+      .attr("y", 200)
+      .style("font-size", 14)
+      .text(function () {
+        return "Highest Wine Rating: " + d.obj.rating;
+      });
+    svg2
+      .append("text")
+      .attr("id", "curr_country_price")
+      .attr("x", 0)
+      .attr("y", 220)
+      .style("font-size", 14)
+      .text(function () {
+        if (d.obj.price) {
+          var price = parseInt(d.obj.price.split('$')[1].split('.')[0])/4;
+          return "Best Wine Price: $" + price;
+        }
+        else
+          return "No price for this selection";
+      });
+    svg2
+      .append("text")
+      .attr("id", "curr_country_action")
+      .attr("x", 0)
+      .attr("y", 240)
+      .style("font-size", 14)
+      .style("font-style", "italic")
+      .text("Click to add country filter.");
+  };
+
+  var add_choropleth = function () {
+    // Map and projection
+    var path = d3.geoPath();
+    var projection = d3.geoMercator()
+      .scale(100)
+      .center([0, 20])
+      .translate([width / 2, height / 2]);
+
+    // Data and color scale
+    var whiteColorScale = d3.scaleThreshold()
+      .domain([0, 3.5, 4, 4.4, 5])
+      .range(d3.schemeBlues[5]);
+    var redColorScale = d3.scaleThreshold()
+      .domain([0, 3.5, 4, 4.4, 5])
+      .range(d3.schemeReds[5]);
+
+    // Load external data and boot
+    d3.queue()
+      .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+      .defer(d3.csv, 'static/data/database.csv', function (d) {
+        if (d.country != "" && d.rating != "") {
+          if (d.country === "United States") {
+            d.country = "USA"
+          }
+          var curr = data.get(d.country) || { "rating": 0, "type": "White wine" };
+          if (+d.rating > +curr.rating) {
+            data.set(d.country, { "rating": +d.rating, "type": d.types, "grape": d.grapes, "price": d.price, "name": d.name });
+          }
+          graph_data.push(d);
+        }
+      })
+      .await(ready);
+
+    function ready(error, topo) {
+      // Draw the map
+      svg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath().projection(projection))
+        // set the color of each country
+        .attr("fill", function (d) {
+          d.obj = data.get(d.properties.name) || { "rating": 0, "type": "White wine" };
+
+          if (d.obj.type === "White wine") {
+            return whiteColorScale(d.obj.rating);
+          } else {
+            return redColorScale(d.obj.rating);
+          }
+        })
+        .on("click", function (d, i) {
+          $('.chosen-select').val(d.properties.name).trigger('chosen:updated');
+        })
+        .on("mouseover", function (d, i) {
+          add_highlight(d);
+        })
+        .on("mouseout", function (d, i) {
+          d3.select("#curr_country_name").remove();
+          d3.select("#curr_country_grape").remove();
+          d3.select("#curr_country_rating").remove();
+          d3.select("#curr_country_price").remove();
+          d3.select("#curr_country_action").remove();
+        });
+    }
+  };
+
+  var add_legend = function () {
+    var circle_attrs = [
+      { x_axis: 10, y_axis: 40, radius: 5, color: "#e41a1c", type: "Red Wine"},
+      { x_axis: 10, y_axis: 70, radius: 5, color: "#377eb8", type: "White Wine" }
+    ];
+
+    // Add legend circles
+    var circles = svg2
+      .selectAll("circle")
+      .data(circle_attrs)
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        return d.x_axis;
+      })
+      .attr("cy", function (d) {
+        return d.y_axis;
+      })
+      .attr("r", function (d) {
+        return d.radius;
+      })
+      .style("fill", function (d) {
+        return d.color;
+      });
+
+    // Add legend text
+    var label_attrs = [
+      { x_axis: 20, y_axis: 0, text: "Wine Types", fontweight: "bold" },
+      { x_axis: 20, y_axis: 30, text: "Red Wine", fontweight: "normal" },
+      { x_axis: 20, y_axis: 60, text: "White Wine", fontweight: "normal" }
+    ];
+
+    svg2
+      .selectAll("text")
+      .data(label_attrs)
+      .enter()
+      .append("text")
+      .attr("y", function (d) {
+        return d.y_axis;
+      })
+      .attr("x", function (d) {
+        return d.x_axis;
+      })
+      .attr("dy", "1em")
+      .style("font-weight", function (d) {
+        return d.fontweight;
+      })
+      .style("left", "0px")
+      .text(function (d) {
+        return d.text;
+      });
+  };
+
+  var public = {
+    add_choropleth: add_choropleth,
+    add_legend: add_legend
+  };
+
+  return public;
+};
+
+wine_viz = wine_viz_lib.choropleth();
+wine_viz.add_choropleth();
+wine_viz.add_legend();
---
+++ v4/static/js/index22.js
@@ -0,0 +1,235 @@
+// Setting the params for margin 
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 550 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
+
+// setup x 
+var xValue = function(d) { return d.price;}, // data -> value
+    xScale = d3.scaleLinear().range([0, width]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    // xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    xAxis = d3.axisBottom(xScale);
+
+// setup y
+var yValue = function(d) { return d.rating;}, // data -> value
+    yScale = d3.scaleLinear().range([height, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    // yAxis = d3.svg.axis().scale(yScale).orient("left");
+    yAxis = d3.axisLeft(yScale);
+    
+
+// add the graph canvas to the body of the webpage
+var svg = d3.select("div#price").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+//Creating the box to add the  informations on price and rating
+var div = d3.select("div#price").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+//Creating the box to add the  informations on price and rating
+var box = d3.select("div#price").append("box")
+    // .attr("class", "tooltip")
+    .style("opacity", 0);
+
+var min_price = 100
+var max_price = 100000
+var ratings = 0
+var selectedCountries = null
+var selectedGrapes = null
+
+function draw(min_price,max_price,ratings,selectedCountries,selectedGrapes){
+// load data
+d3.csv("static/data/result.csv", function(error, data) {
+
+
+  // change string (from CSV) into number format
+  data.forEach(function(d) {
+    d.price = +d.price;
+    d.rating = +d.rating;
+    d.name = d.name;
+    d.region = d.region;
+    d.country = d.country;
+    d.grapes = d.grapes;
+    d.food = d.food;
+    d.winery = d.winery;
+    d.url = d.url;
+//    console.log(d);
+  });
+
+  // filtering price
+  data = data.filter(function(d){return d.price > min_price;})
+  data = data.filter(function(d){return d.price < max_price;})
+
+  // filtering rating
+  data = data.filter(function(d){return d.rating >= ratings;})
+
+  // filtering country
+  data = data.filter(function (e) {
+          if (selectedCountries != null && selectedCountries.length > 0) {
+              if (!selectedCountries.includes(e.country))
+                  return false;
+          }
+          return true;
+      });
+
+  //filtering grapes    
+  data = data.filter(function (e) {
+          if (selectedGrapes != null && selectedGrapes.length > 0) {
+              if (!selectedGrapes.includes(e.grapes))
+                  return false;
+          }
+          return true;
+      });
+
+
+  // don't want dots overlapping axis, so add in buffer to data domain
+  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+  // x-axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("price");
+
+  // y-axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Rating");
+
+  // draw dots
+  svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 1.5)
+      .attr("cx", xMap)
+      .attr("cy", yMap)
+      .on("mouseover", function(d) {
+               div.transition()
+                 .duration(200)
+                 .style("opacity", .9);
+               div.html(d.price+" USD" + "<br>" + d.rating + " Stars"
+                        +"<br>" + d.name)
+                 .style("left", (d3.event.pageX) + "px")
+                 .style("top", (d3.event.pageY - 28) + "px");
+               box.transition()
+                 .duration(200)
+                 .style("opacity", .9);
+               box.html("<br>"+d.price+" USD" + "<br>" + 
+                        d.rating + " Stars" +"<br>" +
+                        d.name +"<br>" +
+                        d.winery +"<br>"+
+                        d.region +"<br>" +
+                        d.grapes +"<br>" +
+                        d.food
+                        )
+                 .style("left", 1000 + "px")
+                 .style("top", 0 + "px")
+                 .style("font-size", 13);
+                                     }
+          )
+     .on("mouseout", function(d) {
+       div.transition()
+         .duration(500)
+         .style("opacity", 0);
+       box.transition()
+         .duration(500)
+         .style("opacity", 0);
+       })
+     .on("click",function(d){
+          window.open(d.url, '_blank')});
+
+// This function is to make the slider work
+function getVals(){
+  // Get slider values
+  var parent = this.parentNode;
+  var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat( slides[0].value );
+    var slide2 = parseFloat( slides[1].value );
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+  
+  var displayElement = parent.getElementsByClassName("rangeValues")[0];
+      displayElement.innerHTML = slide1 + " - " + slide2
+      svg.selectAll("*").remove()
+      svg.selectAll("*").transition().duration(3000)
+      draw(slide1,slide2,ratings,selectedCountries,selectedGrapes);
+      
+      max_price = slide2
+      min_price = slide1
+      console.log(max_price)
+
+  
+}
+
+window.onload = function(){
+  // Initialize Sliders
+  var sliderSections = document.getElementsByClassName("range-slider");
+      for( var x = 0; x < sliderSections.length; x++ ){
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for( var y = 0; y < sliders.length; y++ ){
+          if( sliders[y].type ==="range" ){
+            sliders[y].oninput = getVals;
+            // Manually trigger event first time to display values
+            sliders[y].oninput();
+          }
+        }
+      }
+}
+
+//function to get the changes in the rating filter
+const buttons = d3.selectAll('#rating');
+buttons.on('change', function(d) {
+ratings = this.value/10
+svg.selectAll("*").remove()
+svg.selectAll("*").transition().duration(3000)
+draw(min_price,max_price,ratings,selectedCountries,selectedGrapes)
+console.log(ratings)
+});
+
+//function to get the changes in the country and grapes filter
+$(".chosen-select").chosen({}).change(function (e, c) {
+        // filtering country
+        selectedCountries = Array.from(document.getElementById("country").selectedOptions).map(function (e) {
+            return e.value;
+        });
+        svg.selectAll("*").remove()
+        svg.selectAll("*").transition().duration(3000)
+        draw(min_price,max_price,ratings,selectedCountries,selectedGrapes)
+        
+        // filtering grapes
+        selectedGrapes = Array.from(document.getElementById("grape").selectedOptions).map(function (e) {
+            return e.value;
+        });
+        svg.selectAll("*").remove()
+        svg.selectAll("*").transition().duration(3000)
+        draw(min_price,max_price,ratings,selectedCountries,selectedGrapes)
+
+    });
+
+
+
+
+});
+}
+
+draw(0,10000,0)
